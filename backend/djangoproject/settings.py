03d5937c1d8b328e3c42344f00a2e3dc31020f34
Alex Ye <alexander.ye7@gmail.com>
2025-01-20 20:15:05 -0500

    load_dotenv use default instead of BASE_DIR

--- backend/djangoproject/settings.py
+++ backend/djangoproject/settings.py
@@ -14,11 +14,8 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 
-# Get the path to the directory this file is in
-BASEDIR = os.path.abspath(os.path.dirname(__file__))
-
 # Connect the path with your '.env' file name
-load_dotenv(os.path.join(BASEDIR, '.env'))
+load_dotenv()
 
 DJANGO_SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
